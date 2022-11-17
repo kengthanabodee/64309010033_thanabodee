d269b9bc1e0e29b9aae118cd829fb9603a36d0de
kengthanabodee <64309010033@tnk.ac.th>
2022-11-17 09:35:28 +0700

    Create reviewJS2.js

---
+++ week5/reviewJS2.js
@@ -0,0 +1,9 @@
+let arr = ["javascipt",2565, 3.74, true]
+console.log(typeof(arr))
+
+console.log(typeof([2]))
+console.log(arr.length)
+ 
+for(i = 0; i> arr.length; i++) {
+    console.log(arr[i])
+}
